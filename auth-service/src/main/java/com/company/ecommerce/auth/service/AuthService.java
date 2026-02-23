bf6671432aa1a8cbdfd7784947e24a593b7a987e
Hemant Garg <hanygarg1452@gmail.com>
2026-02-23 11:39:09 +0530

    feat(auth): implement custom authentication and login endpoint

--- auth-service/src/main/java/com/company/ecommerce/auth/service/AuthService.java
+++ auth-service/src/main/java/com/company/ecommerce/auth/service/AuthService.java
@@ -1,11 +1,14 @@
 package com.company.ecommerce.auth.service;
 
+import com.company.ecommerce.auth.dto.LoginRequest;
 import com.company.ecommerce.auth.dto.RegisterRequest;
 import com.company.ecommerce.auth.entity.Role;
 import com.company.ecommerce.auth.entity.User;
 import com.company.ecommerce.auth.repository.RoleRepository;
 import com.company.ecommerce.auth.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -18,6 +21,7 @@ public class AuthService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
     private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
     public void register(RegisterRequest request){
         if(userRepository.existsByEmail(request.getEmail())){
@@ -36,5 +40,14 @@ public class AuthService {
                 .enabled(true)
                 .build();
         userRepository.save(user);
+    }
+
+    public void login(LoginRequest request){
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
     }
 }
